452716280026b9f09e80268947cc9fcac76ec468
zeeshan0987 <zeeshan0987ali@gmail.com>
2023-08-11 15:43:39 +0530

    connect to DB

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const mongoURI ="mongodb://localhost:27017/"
+
+
+const connectDB = async () => {
+    try {
+        mongoose.set('strictQuery', false)
+        mongoose.connect(mongoURI) 
+        console.log('Mongo connected')
+    } catch(error) {
+        console.log(error)
+        process.exit()
+    }
+}
+
+module.exports = connectDB
--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-console.log("hello")
-console.log("hello")
+const connectDB = require("./db")
+const express = require('express')
+
+const app = express()
+const port = 3000
+connectDB()
+
+app.get('/', (req, res) => {
+    res.send('Hello World ')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
